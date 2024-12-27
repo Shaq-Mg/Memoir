af8c119e26079c352585660fc102bcf680bad9b9
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-27 20:17:57 +0000

    App custom colours + User data model

---
+++ App/MemoirApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoirApp.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 27/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Models/Auth/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 27/12/2024.
+//
+
+import Foundation
