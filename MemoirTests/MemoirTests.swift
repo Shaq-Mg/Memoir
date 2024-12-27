54105eafc040dd05714c220490e4f071268826cc
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-27 20:08:33 +0000

    Initial Commit

---
+++ MemoirTests/MemoirTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoirTests.swift
+//  MemoirTests
+//
+//  Created by Shaquille McGregor on 27/12/2024.
+//
+
+import Testing
+@testable import Memoir
+
+struct MemoirTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
