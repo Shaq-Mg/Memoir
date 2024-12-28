3a105938612a7173889cc6796eb2763b8bd2a85b
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-28 23:02:22 +0000

    SETUP: ClientView + search bar view UI

---
+++ Components/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 28/12/2024.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
---
+++ Subviews/Buttons/CreateButton.swift
@@ -0,0 +1,18 @@
+//
+//  CreateButton.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 28/12/2024.
+//
+
+import SwiftUI
+
+struct CreateButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateButton()
+}
