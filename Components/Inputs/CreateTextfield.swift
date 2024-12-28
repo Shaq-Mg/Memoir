a0bc85579a4333f84ea9e13794c7b4a110710b86
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-28 22:32:32 +0000

    CreateClientView with create textfield component

---
+++ Components/Inputs/CreateTextfield.swift
@@ -0,0 +1,18 @@
+//
+//  CreateTextfield.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 28/12/2024.
+//
+
+import SwiftUI
+
+struct CreateTextfield: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateTextfield()
+}
---
+++ Core/Client/CreateClientView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateClientView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 28/12/2024.
+//
+
+import SwiftUI
+
+struct CreateClientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateClientView()
+}
