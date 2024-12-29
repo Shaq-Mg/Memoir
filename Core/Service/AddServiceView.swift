5971945f61184d4ded2e3eba6240462f10b54c35
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-29 17:46:10 +0000

    AddServiceView added

---
+++ Core/Service/AddServiceView.swift
@@ -0,0 +1,18 @@
+//
+//  AddServiceView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 29/12/2024.
+//
+
+import SwiftUI
+
+struct AddServiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddServiceView()
+}
