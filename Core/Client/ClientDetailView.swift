33692e5e6f9a0a3433b0f381c876bfe71cf48863
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-29 00:10:28 +0000

    Navigate to ClientDetailView

---
+++ Core/Client/ClientDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ClientDetailView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 28/12/2024.
+//
+
+import SwiftUI
+
+struct ClientDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClientDetailView()
+}
---
+++ Subviews/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 28/12/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
