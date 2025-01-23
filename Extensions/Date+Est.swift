80cf53841d62ccf20c65e892ad0f2396c9e666f2
Shaq-Mg <shaqmcg0@gmail.com>
2025-01-23 13:22:28 +0000

    Calender data + view model with date extension functions to convert dates into strings

---
+++ Extensions/Date+Est.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Est.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 23/01/2025.
+//
+
+import Foundation
---
+++ Models/Calender.swift
@@ -0,0 +1,8 @@
+//
+//  Calender.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 23/01/2025.
+//
+
+import Foundation
