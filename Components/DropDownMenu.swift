75528dd8356b2c0349775f35e83e4f747d8a554f
Shaq-Mg <shaqmcg0@gmail.com>
2025-01-24 21:29:19 +0000

    Generic selection sheet to display in FormView with drop down menu + also converted Service duration from String to Double using computed property in Service view model (currently experiencing add service view preview crashes 🥴)

---
+++ Components/DropDownMenu.swift
@@ -0,0 +1,18 @@
+//
+//  DropDownMenu.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 24/01/2025.
+//
+
+import SwiftUI
+
+struct DropDownMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DropDownMenu()
+}
---
+++ Components/Headers/MainHeaderView.swift
@@ -0,0 +1,63 @@
+//
+//  MainHeaderView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 28/12/2024.
+//
+
+import SwiftUI
+
+struct MainHeaderView: View {
+    @Binding var showSideMenu: Bool
+    @Environment(\.dismiss) var dismiss
+    var onDismiss = false
+    let title: String
+    
+    var body: some View {
+        VStack {
+            if onDismiss {
+                HStack {
+                    Text("------")
+                        .foregroundStyle(.clear)
+                    Spacer()
+                    Text(title)
+                        .foregroundStyle(.white)
+                        .font(.system(size: 20, weight: .semibold))
+                    Spacer()
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Cancel")
+                            .foregroundStyle(.white)
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                }
+            } else {
+                HStack {
+                    Text("")
+                        .foregroundStyle(.clear)
+                    Spacer()
+                    Text(title)
+                        .foregroundStyle(.white)
+                        .font(.system(size: 20, weight: .semibold))
+                    Spacer()
+                    Button {
+                        showSideMenu.toggle()
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundStyle(.white)
+                            .font(.system(size: 18, weight: .semibold))
+                    }
+                }
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(Color("icon"))
+    }
+}
+
+#Preview {
+    MainHeaderView(showSideMenu: .constant(false), title: "Settings")
+}
+
