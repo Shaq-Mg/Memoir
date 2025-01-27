30566877193b73a3240e690fe13bf2b5e5c6a0d2
Shaq-Mg <shaqmcg0@gmail.com>
2025-01-27 00:10:07 +0000

    Home chat view setup + changing isExpanded into binding property

---
+++ Components/Labels/BookAppointmentLabel.swift
@@ -0,0 +1,18 @@
+//
+//  BookAppointmentLabel.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 26/01/2025.
+//
+
+import SwiftUI
+
+struct BookAppointmentLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookAppointmentLabel()
+}
