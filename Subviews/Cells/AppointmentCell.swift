5beddf9c573445b27c7630f08974ebd8441c1980
Shaq-Mg <shaqmcg0@gmail.com>
2025-01-30 14:53:58 +0000

    DayView UI setup to display appointment for specific date with navigation from chart view

---
+++ Subviews/Cells/AppointmentCell.swift
@@ -0,0 +1,18 @@
+//
+//  AppointmentCell.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 30/01/2025.
+//
+
+import SwiftUI
+
+struct AppointmentCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppointmentCell()
+}
