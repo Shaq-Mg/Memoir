2b29beadab2ff0b868eecfad77bf8aeaedcbd185
Shaq-Mg <shaqmcg0@gmail.com>
2025-01-31 14:33:40 +0000

    Schedule view UI setup 📆

---
+++ Core/Calender/ScheduleView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 31/01/2025.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleView()
+}
