f0affaa0348dc9036c402c08448d6f8ed586b8f4
Shaq-Mg <shaqmcg0@gmail.com>
2025-02-01 21:31:47 +0000

    Side menu view setup with selected option state variable 😉

---
+++ Components/Labels/MenuLabel.swift
@@ -0,0 +1,18 @@
+//
+//  MenuLabel.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 01/02/2025.
+//
+
+import SwiftUI
+
+struct MenuLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuLabel()
+}
