d6e63e819090b4527e2de7291d852b3390a3da86
Shaq-Mg <shaqmcg0@gmail.com>
2025-03-02 23:39:09 +0000

    Displaying side menu with HomeView + RootView for authentication user session

---
+++ Core/Login/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 02/03/2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
---
+++ Core/Menu/MainMenuView.swift
@@ -0,0 +1,8 @@
+//
+//  MainMenuView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 02/03/2025.
+//
+
+import Foundation
