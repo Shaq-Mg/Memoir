86684fe45e17e800dfd470295d12239725387a66
Shaq-Mg <shaqmcg0@gmail.com>
2025-05-11 21:27:32 +0100

    EditProfileView with CircularProfileView UI setup 🔥

---
+++ Components/CircularProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProfileView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 11/05/2025.
+//
+
+import SwiftUI
+
+struct CircularProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProfileView()
+}
