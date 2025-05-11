67c6fdba66fcfb09010d9b4c4c4c48190eb1f8f6
Shaq-Mg <shaqmcg0@gmail.com>
2025-05-11 18:14:31 +0100

    Login + Sign up view UI

---
+++ App/MemoirApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoirApp.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemoirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
