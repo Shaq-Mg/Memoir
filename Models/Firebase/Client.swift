0c71c92c540787adebcbecaabe7287b131879ccf
Shaq-Mg <shaqmcg0@gmail.com>
2025-05-12 17:54:55 +0100

    Client data model + generic FirebaseService with reusable CRUD functions

---
+++ Models/Firebase/Client.swift
@@ -0,0 +1,8 @@
+//
+//  Client.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 12/05/2025.
+//
+
+import Foundation
---
+++ Utilities/FirebaseService.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseService.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 12/05/2025.
+//
+
+import Foundation
