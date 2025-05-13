73730874da6c49c3cfeb48f4b56c8423523666e5
Shaq-Mg <shaqmcg0@gmail.com>
2025-05-13 22:21:14 +0100

    Filter search in ClientManager & presenting and fetching favourite clients

---
+++ Subviews/ClientListView.swift
@@ -0,0 +1,18 @@
+//
+//  ClientListView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 13/05/2025.
+//
+
+import SwiftUI
+
+struct ClientListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClientListView()
+}
