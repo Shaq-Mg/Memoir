3fd8102092f333e706e98bed905fad4b57358cfe
Shaq-Mg <shaqmcg0@gmail.com>
2025-05-16 20:07:55 +0100

    DropDownMenu and FormInputView UI setup

---
+++ Components/Inputs/FormInputView.swift
@@ -0,0 +1,18 @@
+//
+//  FormInputView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 16/05/2025.
+//
+
+import SwiftUI
+
+struct FormInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormInputView()
+}
