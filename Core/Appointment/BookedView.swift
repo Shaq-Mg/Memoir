66f3621d492bbaf87b7fb5b9e293354573973430
Shaq-Mg <shaqmcg0@gmail.com>
2025-05-17 21:34:38 +0100

    Successfully booking appt with reusable selection sheet, presented booking view and stored information to database 🥳

---
+++ Core/Appointment/BookedView.swift
@@ -0,0 +1,18 @@
+//
+//  BookedView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 17/05/2025.
+//
+
+import SwiftUI
+
+struct BookedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookedView()
+}
---
+++ Sheets/SelectionSheet.swift
@@ -0,0 +1,18 @@
+//
+//  SelectionSheet.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 17/05/2025.
+//
+
+import SwiftUI
+
+struct SelectionSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectionSheet()
+}
