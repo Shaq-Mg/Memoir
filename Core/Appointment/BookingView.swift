e1a0fd4b6449dd869fe2d51959c1b10953a63511
Shaq-Mg <shaqmcg0@gmail.com>
2025-05-24 12:44:55 +0100

    Successfully fetched available time slots for specific date and remove booked times from array + BookingView added 🥳

---
+++ Core/Appointment/BookingView.swift
@@ -0,0 +1,18 @@
+//
+//  BookingView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 23/05/2025.
+//
+
+import SwiftUI
+
+struct BookingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookingView()
+}
---
+++ Core/Appointment/ViewModel/BookingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BookingViewModel.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 23/05/2025.
+//
+
+import Foundation
