4206c1e6208fc537fe518f2af470d10f153b0bec
Shaq-Mg <shaqmcg0@gmail.com>
2025-06-03 21:17:06 +0100

    Successfully fetching appts for chart state UI + displaying total earnings for dates header 🥳

---
+++ Core/Chart/Views/ChartStateView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartStateView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 03/06/2025.
+//
+
+import SwiftUI
+
+struct ChartStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartStateView()
+}
