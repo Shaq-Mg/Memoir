e354d26fcab5bc6c1c41357c789234f03dbb0591
Shaq-Mg <shaqmcg0@gmail.com>
2025-06-15 17:27:41 +0100

    ChartManager shared instance for view model and ChartView setup

---
+++ Components/Label/BookApptLabelView.swift
@@ -0,0 +1,18 @@
+//
+//  BookApptLabelView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 15/06/2025.
+//
+
+import SwiftUI
+
+struct BookApptLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookApptLabelView()
+}
---
+++ Core/Chart/Views/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 15/06/2025.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
