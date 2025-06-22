fe4117b99edea920f59af71c22c11bb488f99568
Shaq-Mg <shaqmcg0@gmail.com>
2025-06-22 21:32:22 +0100

    Async delete account function created in auth service/settings view model to remove user from backend + settings sheets setup

---
+++ Sheets/Settings/InformationSheet.swift
@@ -0,0 +1,18 @@
+//
+//  InformationSheet.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 22/06/2025.
+//
+
+import SwiftUI
+
+struct InformationSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InformationSheet()
+}
---
+++ Sheets/Settings/NotificationSheet.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationSheet.swift
+//  Memoir
+//
+//  Created by Shaquille McGregor on 22/06/2025.
+//
+
+import SwiftUI
+
+struct NotificationSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationSheet()
+}
